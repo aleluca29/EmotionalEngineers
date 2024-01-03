a25b0c8992d31b5f152f56c5bb541073b0e3ab6a
bianca <bianca.diaconu0120@gmail.com>
2024-01-03 20:51:45 +0200

    MongoDB connection

--- backend/EmotionalEngineersApp/utils/dbConnection.py
+++ backend/EmotionalEngineersApp/utils/dbConnection.py
@@ -1,20 +1,30 @@
+import logging
 import os
+
 from mongoengine import connect
 
-# Get the password from environment variable
+# Set up basic configuration for logging
+logging.basicConfig(level=logging.INFO)
+
 mongodb_password = os.getenv('MONGO_DB_PASSWORD')
 
-# If using f-string, ensure you handle the case where the environment variable might not be set
 if mongodb_password is None:
     raise ValueError("MongoDB password not set in environment variables")
 
 mongodb_connection_string = (
     f'mongodb://MSAalebia:{mongodb_password}@'
     'msa.cluster-clx6f6bdbck7.eu-west-3.docdb.amazonaws.com:27017/?'
+    'tls=true&tlsCAFile=utils/global-bundle.pem'
 )
 
 
 def init_db():
-    connect('msa', host=mongodb_connection_string)
+    try:
+        connect('msa', host=mongodb_connection_string)
+        logging.info("Connected to MongoDB successfully.")
+    except Exception as e:
+        logging.error(f"Failed to connect to MongoDB: {e}")
 
 
+# Initialize the database connection
+init_db()
